a14da70dae6c16af0b4c96cdd31a3bddeb31c737
Yasin Arafat <mdyasinarafat3@gmail.com>
2023-08-01 02:23:44 +0200

    Create Input and Output.cpp

---
+++ hackerrank/C++/Input and Output.cpp
@@ -0,0 +1,17 @@
+// Input and Output
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a, b, c, sum;
+    cin >> a >> b >> c;
+    sum = a + b + c;
+    cout << sum;
+    return 0;
+}
