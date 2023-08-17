5ad35acadac3373624c56f55bd8f3e9364d59641
Yasin Arafat <mdyasinarafat3@gmail.com>
2023-08-17 09:30:43 +0200

    Create missing-numbers.cpp

---
+++ hackerrank/Algorithms/Search/missing-numbers.cpp
@@ -0,0 +1,18 @@
+// Missing Numbers
+// https://www.hackerrank.com/challenges/missing-numbers/problem
+vector<int> missingNumbers(vector<int> arr, vector<int> brr)
+{
+    vector<int> num;
+    sort(arr.begin(), arr.end());
+    sort(brr.begin(), brr.end());
+    for (int i = 0; i < brr.size(); i++)
+    {
+        if (arr[i] != brr[i])
+        {
+            arr.insert(arr.begin(), 0);
+            num.push_back(brr[i]);
+        }
+    }
+    num.erase(unique(num.begin(), num.end()), num.end());
+    return num;
+}
